d29922c5aafdc81b9a8e580d67238dc79a89ad09
Use your GitHub username <uko20001@byui.edu>
2022-05-19 01:27:02 +0100

    open commerce

---
+++ chamber/js/modify.js
@@ -0,0 +1,18 @@
+document.querySelector('#datemod').textContent = document.lastModified;
+document.querySelector('#year').textContent = new Date().getFullYear(); 
+// select the elements to manipulate (output to)
+const datefield = document.querySelector("#currentDate");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
