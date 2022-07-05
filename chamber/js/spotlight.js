a0aaad474e0b6349eba91940ca25b5ede91ca9a1
Use your GitHub username <uko20001@byui.edu>
2022-07-05 10:28:52 +0100

    open chamber

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,50 +1,68 @@
-const requestURL = 'json/data.json';
-const spotlight1 = document.querySelector('.spotLight1');
+const requestURL = "json/data.json";
+let cards = document.querySelector(".cards");
+let count = 0;
+let results = [];
 
-async function displaydirectory() {
-  let response = await fetch(requestURL);
-  if (response.ok) {
-    let data = await response.json();
-    builddirectory(data);
-  } else {
-  throw Error(response.statusText);
+function builddirectory(directory) {
+  let section = document.createElement("section");
+  section.classList.add("spotlights");
+  let image = document.createElement("img");
+  let name = document.createElement("h3");
+  let address = document.createElement("p");
+  let phone = document.createElement("p");
+  let membership = document.createElement("p");
+  membership.classList.add("levels");
+  let a = document.createElement("a");
+  let link = document.createTextNode("visit site");
+
+  image.setAttribute("src", directory.imageurl);
+  image.setAttribute("alt", `Logo of ${directory.name}`);
+  image.setAttribute("loading", "lazy");
+  // Change the textContent property to contain the member's full name, address, phone, email
+  name.innerHTML = directory.name;
+  address.innerHTML = directory.address;
+  phone.innerHTML = directory.phone;
+  membership.innerHTML = `${directory.membership} Member`;
+  a.append(link);
+  a.title = "visit site";
+  a.href = directory.website;
+
+  section.append(name);
+  section.append(address);
+  section.append(phone);
+  section.append(membership);
+  section.append(image);
+  section.append(a);
+
+  if (count == 6) {
+    return;
+  } else if (count <= 5) {
+    if (directory.membership == "Gold") {
+      results[count] = cards.append(section);
+      count += 1;
+    }
+    if (directory.membership == "Silver") {
+      results[count] = cards.append(section);
+
+      count = count + 1;
+    }
+    section.setAttribute("id", `spotLight${count}`);
   }
 }
 
-function builddirectory(data) {  
-    let choice = data.directory.filter(p => p.membership === 'Gold'|| p.membership === 'Silver')
-    choice.forEach(directory => {
-    let section = document.createElement('section');
-    let image = document.createElement('img');
-    let name = document.createElement('h3');
-    let address = document.createElement('p');
-    let phone = document.createElement('p');
-    let membership = document.createElement('p');
-    let a = document.createElement('a');
-    let link = document.createTextNode('visit site');
-
-    
-    image.setAttribute('src', directory.imageurl );
-    image.setAttribute('alt', `logo of ${directory.name}`);
-    image.setAttribute('loading', 'lazy');
-    name.innerHTML = directory.name;
-    address.innerHTML = directory.address;
-    phone.innerHTML = directory.phone;
-    membership.innerHTML = directory.membership;
-    a.append(link);
-    a.title = 'visit site';
-    a.href = directory.website;
-
-    section.append(image);
-    section.append(name);
-    section.append(address);
-    section.append(phone);
-    section.append(membership);
-    section.append(a);
-
-    spotlight1.append(section);
-    
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
   })
-}
-displaydirectory();
+  .then(function (jsonObject) {
+    const members = jsonObject["members"];
+    members.forEach(builddirectory);
 
+    let randomItem = results[Math.floor(Math.random() * [results.length - 1])];
+    randomItem.style.display = "none";
+    let randomItem2 = results[Math.floor(Math.random() * [results.length - 1])];
+    while (randomItem == randomItem2) {
+      randomItem2 = results[Math.floor(Math.random() * [results.length - 1])];
+    }
+    randomItem2.style.display = "none";
+  });
